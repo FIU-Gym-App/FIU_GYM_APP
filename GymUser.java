69fb15826574fd6ecd75a7efb24d3a01c6ee8697
nicolec082202 <97425835+nicolec082202@users.noreply.github.com>
2022-03-17 17:47:55 -0400

    Update GymUser.java

--- GymUser.java
+++ GymUser.java
@@ -3,11 +3,12 @@ package app;
 
 public class GymUser {
     private boolean checkedIn;
-    private int numberOfCheckedIn;
+    private int numberOfCheckedIn = 0;
     
 
     public GymUser(){
         checkedIn = true;
+        numberOfCheckedIn++;
         
     }
 
