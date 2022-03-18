40a9f62a673fbabf5688f1f5f2c0c6e515bf6a53
nicolec082202 <97425835+nicolec082202@users.noreply.github.com>
2022-03-17 22:21:51 -0400

    Update TrendCalculator.java

--- TrendCalculator.java
+++ TrendCalculator.java
@@ -3,7 +3,7 @@ import java.util.Timer;
 
 public class TrendCalculator  {
     final int GYM_LIMIT = 500;
-    int numberOfChechedIn = getNumberOfCheckedIn();
+    int numberOfChechedIn = GymUser.getNumberOfCheckedIn();
 
     public double TrendCalculator(int numberOfChechedIn){
         return numberOfChechedIn/GYM_LIMIT;
