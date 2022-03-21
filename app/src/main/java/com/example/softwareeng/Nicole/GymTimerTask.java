dcb0240d7f63a79c6c902c86920aa48c698f6c71
Johnny <johnny.casares.7@gmail.com>
2022-03-21 18:14:35 -0400

    used nicoles timer xd

--- app/src/main/java/com/example/softwareeng/Nicole/GymTimerTask.java
+++ app/src/main/java/com/example/softwareeng/Nicole/GymTimerTask.java
@@ -1,15 +1,27 @@
 package com.example.softwareeng.Nicole;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class GymTimerTask extends TimerTask{
 
+
      Timer timer = new Timer();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public void run(){
         GymUser.setCheckedIn(false);
         System.out.println(GymUser.getCheckedIn());
         GymUser.setNumberOfCheckedIn(GymUser.getNumberOfCheckedIn()- 1);
         timer.cancel();
+        //after time has finished checkout the user
+        //get collection and document path
+        DocumentReference checkIn = db.collection("checkInCounter").document("checkIn");
+        // Atomically increment the population by 1
+        checkIn.update("checkIn", FieldValue.increment(-1));
+
     }
 }
--- app/src/main/java/com/example/softwareeng/Nicole/MiddleMan.java
+++ app/src/main/java/com/example/softwareeng/Nicole/MiddleMan.java
@@ -13,19 +13,6 @@ public class MiddleMan {
         GymTimer timer = new GymTimer(5);
 
 
-        //when timer  = 0,  meaning that the 5 seconds have passed, subtract one from the data base
-        //Timer that works xd
-        new java.util.Timer().schedule(
-                new java.util.TimerTask() {
-                    @Override
-                    public void run() {
-                        DocumentReference checkIn = db.collection("checkInCounter").document("checkIn");
-                        // Atomically increment the population by 1
-                        checkIn.update("checkIn", FieldValue.increment(-1));
-                    }
-                },
-                5000
-        );
 
 
     }
