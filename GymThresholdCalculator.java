7b2a77f47ad9dc9a9adebd9ee768774f61b1cc06
ylmzzertann <100012013+ylmzzertann@users.noreply.github.com>
2022-03-21 00:32:55 -0400

    Add files via upload

---
+++ GymThresholdCalculator.java
@@ -0,0 +1,23 @@
+
+public class GymThresholdCalculator extends GymUser
+{
+	public int maxUser = 100;
+	public int numberOfCheckedIn = 0; 
+ public GymThresholdCalculator() {
+	 GymUser.setCheckedIn(true);
+	 System.out.println(GymUser.getCheckedIn());
+	    GymUser.setNumberOfCheckedIn(GymUser.getNumberOfCheckedIn()+ 1);
+	    numberOfCheckedIn = numberOfCheckedIn +1;
+	    
+	   
+	 /*   
+	    if(numberOfCheckedIn >= maxUser * 60 / 100)
+	    	
+	    {
+	    	System.out.println("the population reached 60% capacity ");
+	    }
+	    
+	   */
+	   
+ }
+}
