72913fffbd169933c00b73b5b43ab71737b41947
nicolec082202 <97425835+nicolec082202@users.noreply.github.com>
2022-04-05 17:28:52 -0400

    Update GymTimerTask.java

--- GymTimerTask.java
+++ GymTimerTask.java
@@ -9,7 +9,6 @@ public class GymTimerTask extends TimerTask{
     public void run(){
         GymUser.setCheckedIn(false);
         GymUser.setCheckedOut(true);
-        GymUser.setNumberOfCheckedIn(GymUser.getNumberOfCheckedIn()- 1);
         GymUser.setNumberOfCheckedOut(GymUser.getNumberOfCheckedOut()+1);
         timer.cancel();
     }
